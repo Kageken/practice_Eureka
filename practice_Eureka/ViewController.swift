1aba0f649326c85e51b901dd417ee87a56dcd1ac
陰山賢太 <kageken@kageyamakentanomacbook-puro.local>
2018-06-27 10:15:09 +0900

    Initial Commit

---
+++ practice_Eureka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice_Eureka
+//
+//  Created by 陰山賢太 on 2018/06/27.
+//  Copyright © 2018年 Kageken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
